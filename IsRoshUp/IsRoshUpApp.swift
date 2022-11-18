fd8884a73f3336055a660e2283def7c2c9f9c475
Joshua Moody <jmoody@github.com>
2022-11-18 12:59:49 +0100

    initial commit

---
+++ IsRoshUp/IsRoshUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsRoshUpApp.swift
+//  IsRoshUp
+//
+//  Created by moody on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct IsRoshUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
